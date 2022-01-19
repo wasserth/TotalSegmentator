6f970e2b0f68298d0d090657c0e4db6522eff515
wasserth <j.wasserthal@gmx.de>
2022-01-19 13:31:29 +0100

    basic package setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name='TotalSegmentator',
+        version='0.1',
+        description='Robust segmentation of X classes in CT images.',
+        long_description="See Readme.md on github for more details.",
+        url='https://github.com/wasserth/TotalSegmentator',
+        author='Jakob Wasserthal',
+        author_email='jakob.wasserthal@usb.ch',
+        python_requires='>=3.5',
+        license='Apache 2.0',
+        packages=find_packages(),
+        install_requires=[
+            'numpy',
+            'nibabel>=2.3.0',
+            'tqdm'
+        ],
+        zip_safe=False,
+        classifiers=[
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: Unix',
+            'Operating System :: MacOS'
+        ],
+        scripts=[
+            'bin/TotalSegmentator'
+        ]
+    )
