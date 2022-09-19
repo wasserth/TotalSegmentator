b3e9cea237c815fcd38308e93d6455b5884e6e1e
wasserth <j.wasserthal@gmx.de>
2022-09-19 14:16:53 +0200

    adapt statistics tests
    Former-commit-id: 938f76e886825cfd549266630d53f2986f5631c2

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -42,15 +42,27 @@ class test_end_to_end(unittest.TestCase):
 
     def test_statistics(self):
         stats_ref = json.load(open("tests/reference_files/example_seg_fast/statistics.json", "r"))
+        stats_ref = pd.DataFrame(stats_ref)
         stats_new = json.load(open("tests/unittest_prediction_fast/statistics.json", "r"))
-        stats_equal = stats_ref == stats_new
-        self.assertTrue(stats_equal, "statistics are not correct")
+        stats_new = pd.DataFrame(stats_new)
+        stats_equal = np.allclose(stats_ref.loc["volume"].values, stats_new.loc["volume"].values,
+                                  rtol=3e-3, atol=3e-3)
+        self.assertTrue(stats_equal, "volume statistics are not correct")
+        stats_equal = np.allclose(stats_ref.loc["intensity"].values, stats_new.loc["intensity"].values,
+                                  rtol=3e-3, atol=3e-3)
+        self.assertTrue(stats_equal, "intensity statistics are not correct")
 
     def test_radiomics(self):
         stats_ref = json.load(open("tests/reference_files/example_seg_fast/statistics_radiomics.json", "r"))
+        stats_ref = pd.DataFrame(stats_ref)
+        stats_ref = stats_ref.fillna(0)
         stats_new = json.load(open("tests/unittest_prediction_fast/statistics_radiomics.json", "r"))
-        stats_equal = stats_ref == stats_new
-        self.assertTrue(stats_equal, "statistics are not correct")
+        stats_new = pd.DataFrame(stats_new)
+        stats_new = stats_new.fillna(0)
+        # very big margin, but some of the radiomics features can change a lot if only a few voxels
+        # of the segmentation change. So this test is only to check that radiomics ran sucessfully.
+        stats_equal = np.allclose(stats_ref.values, stats_new.values, rtol=3e-1, atol=3e-1)
+        self.assertTrue(stats_equal, "radiomics is not correct")
 
 
 if __name__ == '__main__':
