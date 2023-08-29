1d40cede6c20f0f2b1b475d13fed9f043f8a14c5
wasserth <j.wasserthal@gmx.de>
2023-08-29 14:57:32 +0200

    bugfix

--- tests.sh
+++ tests.sh
@@ -5,7 +5,7 @@ set -e
 
 
 # Test multilabel prediction
-TotalSegmentator -i tests/reference_files/example_ct_sm.nii.gz -o tests/unittest_prediction.nii.gz -bs --ml
+TotalSegmentator -i tests/reference_files/example_ct_sm.nii.gz -o tests/unittest_prediction.nii.gz -bs --ml -d cpu
 pytest -v tests/test_end_to_end.py::test_end_to_end::test_prediction_multilabel
 
 # Test organ prediction - roi subset
