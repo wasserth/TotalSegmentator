d70479bcc425f8f6a5a7b0f771d897e1a68c8ea1
wasserth <j.wasserthal@gmx.de>
2023-10-19 11:44:56 +0200

    Bump version to 2.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 
 setup(name='TotalSegmentator',
-        version='2.0.3',
+        version='2.0.4',
         description='Robust segmentation of 104 classes in CT images.',
         long_description="See Readme.md on github for more details.",
         url='https://github.com/wasserth/TotalSegmentator',
