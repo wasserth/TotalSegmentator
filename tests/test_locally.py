a9aedb17a91ace9fcf4eead0fd417983ae962dbd
wasserth <j.wasserthal@gmx.de>
2023-10-27 12:01:30 +0200

    add test_locally (2)

--- tests/test_locally.py
+++ tests/test_locally.py
@@ -1,11 +1,10 @@
 import sys
 from pathlib import Path
-# p_dir = str(Path(__file__).absolute().parents[1])
-# if p_dir not in sys.path: sys.path.insert(0, p_dir)
 
 from pathlib import Path
 import pytest
 import os
+import re
 import sys
 import glob
 import shutil
@@ -13,6 +12,11 @@ import subprocess
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
+from collections import defaultdict
+import psutil
+import time
+import threading
+import torch
 
 from totalsegmentator.python_api import totalsegmentator
 from totalsegmentator.map_to_binary import class_map
@@ -23,37 +27,170 @@ Run a complete prediction locally with GPU and evaluate Dice score.
 This is not possible on github actions due to missing GPU.
 
 Info:
-To create the multilable groundtruth files for 3mm use 
-python ~/dev/jakob_scripts/multiseg/eval/create_3mm_multilabel_file.py
+To get the CT file and create the multilable groundtruth files use 
+python ~/dev/jakob_scripts/multiseg/eval/get_data_for_test_locally.py
 """
 
+
+max_memory_usage = 0  # Initialize max_memory_usage for RAM as 0
+max_gpu_memory_usage = 0  # Initialize max_gpu_memory_usage for GPU as 0
+cpu_utilizations = []  # Initialize an empty list to store CPU utilizations
+gpu_utilizations = []  # Initialize an empty list to store GPU utilizations
+
+def get_cpu_utilization():
+    cpu_util = psutil.cpu_percent(interval=1)  # Get CPU utilization as a percentage
+    cpu_utilizations.append(cpu_util)
+
+def cpu_utilization_monitor(interval=1):
+    while True:
+        get_cpu_utilization()
+        time.sleep(interval)
+
+def get_gpu_utilization():
+    try:
+        sp = subprocess.Popen(["nvidia-smi", "-q", "-i", "0", "-d", "UTILIZATION"],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out_str = sp.communicate()
+        out_list = str(out_str[0]).split('\\n')
+
+        for item in out_list:
+            if "Gpu" in item:
+                m = re.search(r"(\d+)", item)
+                if m:
+                    gpu_util = int(m.group(1))
+                    gpu_utilizations.append(gpu_util)
+                    break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def gpu_utilization_monitor(interval=1):
+    while True:
+        get_gpu_utilization()
+        time.sleep(interval)
+
+def get_memory_usage():
+    global max_memory_usage
+    process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
+    current_memory = memory_info.rss / (1024 ** 2)  # Convert to MB
+    max_memory_usage = max(max_memory_usage, round(current_memory))  # Update max_memory_usage
+
+def get_gpu_memory_usage():
+    global max_gpu_memory_usage
+    current_memory = torch.cuda.memory_allocated() / (1024 ** 2)  # Convert to MB
+    max_gpu_memory_usage = max(max_gpu_memory_usage, round(current_memory))  # Update max_gpu_memory_usage
+
+def reset_monitors():
+    global max_memory_usage
+    global max_gpu_memory_usage
+    global cpu_utilizations
+    global gpu_utilizations
+    max_memory_usage = 0
+    max_gpu_memory_usage = 0
+    cpu_utilizations = []
+    gpu_utilizations = []
+
+def memory_monitor(interval=1):
+    while True:
+        get_memory_usage()
+        time.sleep(interval)
+
+def gpu_memory_monitor(interval=1):
+    while True:
+        get_gpu_memory_usage()
+        time.sleep(interval)
+
+def start_monitors():
+    # Create separate threads to monitor memory usage
+    memory_thread = threading.Thread(target=memory_monitor)
+    memory_thread.daemon = True  
+    memory_thread.start()
+
+    gpu_memory_thread = threading.Thread(target=gpu_memory_monitor)
+    gpu_memory_thread.daemon = True  
+    gpu_memory_thread.start()
+
+    cpu_util_thread = threading.Thread(target=cpu_utilization_monitor)
+    cpu_util_thread.daemon = True
+    cpu_util_thread.start()
+
+    gpu_util_thread = threading.Thread(target=gpu_utilization_monitor)
+    gpu_util_thread.daemon = True  
+    gpu_util_thread.start()
+
+
 if __name__ == "__main__":
+    start_monitors()
 
-    # Todo: add test files as release to github and download each time this is run
+    base_dir = Path("/mnt/nvme/data/multiseg/test_locally")
 
-    img_dir = Path("/mnt/nvme/data/multiseg/test_locally")
-    gt_dir = Path("/mnt/nvme/data/multiseg/test_locally/gt")
-    pred_dir = Path("/mnt/nvme/data/multiseg/test_locally/pred")
+    res_mean = defaultdict(dict)
+    times = defaultdict(list)
+    memory_ram = {}
+    memory_gpu = {}
+    cpu_utilization = {}
+    gpu_utilization = {}
 
-    for img_fn in tqdm(img_dir.glob("*.nii.gz")):
-        totalsegmentator(img_fn, pred_dir / img_fn.name, fast=True, ml=True, device="gpu")
+    for resolution in ["15mm", "3mm"]:
+    # for resolution in ["3mm"]:
+        img_dir = base_dir / resolution / "ct"
+        gt_dir = base_dir / resolution / "gt"
+        pred_dir = base_dir / resolution / "pred"
 
-    print("Calc metrics...")
-    subjects = [s.name.split(".")[0] for s in img_dir.glob("*.nii.gz")]
-    res = [calc_metrics(s, gt_dir, pred_dir, class_map["total"]) for s in subjects]
-    res = pd.DataFrame(res)
+        print("Run totalsegmentator...")
+        reset_monitors()
+        for img_fn in tqdm(img_dir.glob("*.nii.gz")):
+            st = time.time()
+            totalsegmentator(img_fn, pred_dir / img_fn.name, fast=True, ml=True, device="gpu")
+            times[resolution].append(time.time()-st)
+
+        print("Logging...")
+        times[resolution] = np.mean(times[resolution]).round(1)
+        memory_ram[resolution] = max_memory_usage
+        memory_gpu[resolution] = max_gpu_memory_usage
+        cpu_utilization[resolution] = np.mean(cpu_utilizations).round(1)
+        gpu_utilization[resolution] = np.mean(gpu_utilizations).round(1)
+
+        print("Calc metrics...")
+        subjects = [s.name.split(".")[0] for s in img_dir.glob("*.nii.gz")]
+        res = [calc_metrics(s, gt_dir, pred_dir, class_map["total"]) for s in subjects]
+        res = pd.DataFrame(res)
+        
+        print("Aggregate metrics...")
+        for metric in ["dice", "surface_dice_3"]:
+            res_all_rois = []
+            for roi_name in class_map["total"].values():
+                row_wo_nan = res[f"{metric}-{roi_name}"].dropna()
+                res_all_rois.append(row_wo_nan.mean())
+                # print(f"{roi_name} {metric}: {row_wo_nan.mean():.3f}")  # per roi
+            # print(f"{metric}: {np.nanmean(res_all_rois):.3f}")  # mean over all rois
+            res_mean[resolution][metric] = np.nanmean(res_all_rois).round(3)
+
+    res_mean = dict(res_mean)
+    times = dict(times)
+
+    print("Scores:")
+    print(res_mean)
+    print("Times (s):")
+    print(times)
+    print("Memory RAM (MB):")
+    print(memory_ram)
+    print("Memory GPU (MB):")
+    print(memory_gpu)
+    print("CPU utilizations (%):")
+    print(cpu_utilization)
+    print("GPU utilizations (%):")
+    print(gpu_utilization)
+
+    reference_res = {"15mm": {"dice": 0.854, "surface_dice_3": 0.97}, 
+                      "3mm": {"dice": 0.907, "surface_dice_3": 0.988}}
+    reference_times = {}
+    reference_memory_ram = {}
+    reference_memory_gpu = {}
+    reference_cpu_utilizations = {}
+    reference_gpu_utilizations = {}
+
+    # Check if Dice + NSD scores are identical
+    # assert dict(res_mean) == reference_res
     
-    print("Print results...")
-    for metric in ["dice", "surface_dice_3"]:
-        res_all_rois = []
-        for roi_name in class_map["total"].values():
-            row_wo_nan = res[f"{metric}-{roi_name}"].dropna()
-            res_all_rois.append(row_wo_nan.mean())
-            # print(f"{roi_name} {metric}: {row_wo_nan.mean():.3f}")  # per roi
-        print(f"{metric}: {np.nanmean(res_all_rois):.3f}")  # mean over all rois
-
-    # pytest.main(['-v', 'tests/test_end_to_end.py::test_end_to_end::test_prediction_fast'])
-    
-    shutil.rmtree(pred_dir)
-
-    # todo: also track runtime and memory consumption!!
+    # shutil.rmtree(pred_dir)
