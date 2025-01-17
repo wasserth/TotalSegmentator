7a79765431bfef2bbc27ca11a08e83148af498d2
jakob <j.wasserthal@gmx.de>
2025-01-17 11:37:01 +0100

    activate docker build in release script

--- resources/release.sh
+++ resources/release.sh
@@ -44,9 +44,7 @@ python setup.py sdist bdist_wheel
 twine upload --skip-existing dist/*
 
 # Step 5: Build and Push Docker Image
-#   (random error on my local machine; have to run on apollo server)
-# docker build -t wasserth/totalsegmentator:${new_version} .
-# docker push wasserth/totalsegmentator:${new_version}
-echo "Build and upload docker container manually"
+docker build -t wasserth/totalsegmentator:${new_version} .
+docker push wasserth/totalsegmentator:${new_version}
 
 echo "Release process completed."
