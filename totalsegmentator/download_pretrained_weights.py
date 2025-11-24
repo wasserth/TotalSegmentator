32749ea9554b244a72152cfd3ca1abd808a8280b
jakob <j.wasserthal@gmx.de>
2025-11-24 14:44:15 +0100

    reduce size of dockerfile from 38gb to 16gb

--- totalsegmentator/download_pretrained_weights.py
+++ totalsegmentator/download_pretrained_weights.py
@@ -1,6 +1,7 @@
 from time import sleep
 
 from libs import download_pretrained_weights
+from config import setup_totalseg, set_config_key
 
 if __name__ == "__main__":
     """
@@ -10,5 +11,9 @@ if __name__ == "__main__":
                     315, 299, 300, 850, 851, 852, 853, 775, 776, 777, 778,
                     779, 351, 913, 789, 527, 552, 570, 576, 115, 952, 113,
                     343]:
+
+        setup_totalseg()
+        set_config_key("statistics_disclaimer_shown", True)
+
         download_pretrained_weights(task_id)
         sleep(5)
