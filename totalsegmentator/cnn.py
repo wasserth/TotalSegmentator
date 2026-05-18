87ca8c2e15b18948d73db8f063715da3d202bf82
jakob <j.wasserthal@gmx.de>
2026-05-18 15:58:32 +0200

    update cnn models

--- totalsegmentator/cnn.py
+++ totalsegmentator/cnn.py
@@ -14,21 +14,35 @@ DEFAULT_BODY_STATS_CNN_ROOT_DIR = get_weights_dir() / "lightning_models"
 DEFAULT_BODY_STATS_CNN_DIRS = {
     "mr": {
 
+        # older
         # "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_weight_splitXGB_2d_ns5_effnetv2",
         # "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_weight_splitOrig_2d_ns5_effnetv2",
-        "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_weight_splitXGB_2d_ns5_mo1_effnetv2",
-        "size": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_size_splitXGB_2d_ns5_mo1_effnetv2",
-        "age": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_age_splitXGB_2d_ns5_mo1_effnetv2",
-        "sex": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_sex_splitXGB_2d_ns5_mo1_effnetv2",
+        # old
+        # "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_weight_splitXGB_2d_ns5_mo1_effnetv2",
+        # "size": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_size_splitXGB_2d_ns5_mo1_effnetv2",
+        # "age": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_age_splitXGB_2d_ns5_mo1_effnetv2",
+        # "sex": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_sex_splitXGB_2d_ns5_mo1_effnetv2",
+        # current
+        "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_weight_splitOrig_2d_ns5_effnetv2",
+        "size": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_size_splitOrig_2d_ns5_effnetv2",
+        "age": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_age_splitOrig_2d_ns5_effnetv2",
+        "sex": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "mr_sex_splitOrig_2d_ns5_effnetv2",
     },
     "ct": {
+        # older
         # can not use older mo1 models, because they are based on sparse z-sampling which is not done during inference
         # "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_weight_splitXGB_2d_ns5_effnetv2_ep40",
-        "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "old_2/ct_weight_splitOrig_2d_ns5_effnetv2",
+        # "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "old_2/ct_weight_splitOrig_2d_ns5_effnetv2",
+        # old
         # "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_weight_splitXGB_2d_ns5_mo1_effnetv2_npy0",
-        "size": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_size_splitXGB_2d_ns5_mo1_effnetv2_npy0",
-        "age": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_age_splitXGB_2d_ns5_mo1_effnetv2_npy0",
-        "sex": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_sex_splitXGB_2d_ns5_mo1_effnetv2_npy0",
+        # "size": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_size_splitXGB_2d_ns5_mo1_effnetv2_npy0",
+        # "age": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_age_splitXGB_2d_ns5_mo1_effnetv2_npy0",
+        # "sex": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_sex_splitXGB_2d_ns5_mo1_effnetv2_npy0",
+        # current
+        "weight": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_weight_splitOrig_2d_ns5_effnetv2",
+        "size": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_size_splitOrig_2d_ns5_effnetv2",
+        "age": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_age_splitOrig_2d_ns5_effnetv2",
+        "sex": DEFAULT_BODY_STATS_CNN_ROOT_DIR / "ct_sex_splitOrig_2d_ns5_effnetv2",
     },
 }
 BODY_STATS_CNN_DOWNLOAD_TASKS = {
