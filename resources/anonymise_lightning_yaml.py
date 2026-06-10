9d92269ce824bd9b44a8bd1f9abc3702df0fe575
jakob <j.wasserthal@gmx.de>
2026-06-10 11:13:52 +0200

    update comment

--- resources/anonymise_lightning_yaml.py
+++ resources/anonymise_lightning_yaml.py
@@ -52,14 +52,14 @@ if __name__ == "__main__":
 
     Then zip all:
     
-    zip -r mr_weight_splitOrig_2d_ns5_effnetv2_fl1.zip mr_weight_splitOrig_2d_ns5_effnetv2_fl1;\
-    zip -r mr_size_splitOrig_2d_ns5_effnetv2_fl1.zip mr_size_splitOrig_2d_ns5_effnetv2_fl1;\
-    zip -r mr_age_splitOrig_2d_ns5_effnetv2_fl1.zip mr_age_splitOrig_2d_ns5_effnetv2_fl1;\
-    zip -r mr_sex_splitOrig_2d_ns5_effnetv2_fl1.zip mr_sex_splitOrig_2d_ns5_effnetv2_fl1;\
-    zip -r ct_weight_splitOrig_2d_ns5_effnetv2_fl1.zip ct_weight_splitOrig_2d_ns5_effnetv2_fl1;\
-    zip -r ct_size_splitOrig_2d_ns5_effnetv2_fl1.zip ct_size_splitOrig_2d_ns5_effnetv2_fl1;\
-    zip -r ct_age_splitOrig_2d_ns5_effnetv2_fl1.zip ct_age_splitOrig_2d_ns5_effnetv2_fl1;\
-    zip -r ct_sex_splitOrig_2d_ns5_effnetv2_fl1.zip ct_sex_splitOrig_2d_ns5_effnetv2_fl1
+    zip -r mr_weight_splitOrig_2d_ns5_effnetv2_fl1_se1.zip mr_weight_splitOrig_2d_ns5_effnetv2_fl1_se1;\
+    zip -r mr_size_splitOrig_2d_ns5_effnetv2_fl1_se1.zip mr_size_splitOrig_2d_ns5_effnetv2_fl1_se1;\
+    zip -r mr_age_splitOrig_2d_ns5_effnetv2_fl1_se1.zip mr_age_splitOrig_2d_ns5_effnetv2_fl1_se1;\
+    zip -r mr_sex_splitOrig_2d_ns5_effnetv2_fl1_se1.zip mr_sex_splitOrig_2d_ns5_effnetv2_fl1_se1;\
+    zip -r ct_weight_splitOrig_2d_ns5_effnetv2_fl1_se1.zip ct_weight_splitOrig_2d_ns5_effnetv2_fl1_se1;\
+    zip -r ct_size_splitOrig_2d_ns5_effnetv2_fl1_se1.zip ct_size_splitOrig_2d_ns5_effnetv2_fl1_se1;\
+    zip -r ct_age_splitOrig_2d_ns5_effnetv2_fl1_se1.zip ct_age_splitOrig_2d_ns5_effnetv2_fl1_se1;\
+    zip -r ct_sex_splitOrig_2d_ns5_effnetv2_fl1_se1.zip ct_sex_splitOrig_2d_ns5_effnetv2_fl1_se1
     
     """
     if len(sys.argv) != 2:
